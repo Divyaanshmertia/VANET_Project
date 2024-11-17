c2d6f8a043193ebfe665c96f9b023291f53f0ca2
Divyaansh Mertia <divyaanshmertia@gmail.com>
2024-11-17 18:28:39 +0530

    Initial commit

---
+++ test_udacity_gaia.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python3 test.py driving_udacity --network_name gaia --architecture ring --model FADNet --n_rounds 3600 --bz_train 32 --bz_test 32 --device cuda --log_freq 100 --local_steps 1 --lr 0.001 --decay sqrt --beta_supp 0.5 --test --save_logg_path /DATA/m23cse013/FADNet/loggs/experiment_driving_udacity_network_name_gaia_fit_by_epoch_False_bz_train_32_lr_0.001_decay_sqrt_local_steps_1/ring
---
+++ train_gazebo_gaia.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python3 main.py driving_gazebo --network_name gaia --architecture ring --model FADNet_plus --n_rounds 1000 --bz_train 32 --bz_test 32 --device cuda --log_freq 40 --local_steps 1 --lr 0.0001 --decay sqrt
